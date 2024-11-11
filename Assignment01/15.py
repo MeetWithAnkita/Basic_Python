3e44d0ca4f29cb63bc71bbd917cb01c380f85f41
Ankita Das <adas22156@gmail.com>
2024-11-11 14:22:54 +0530

    complete

--- Assignment01/15.py
+++ Assignment01/15.py
@@ -5,3 +5,25 @@
 # the basic pay is deducted from the gross salary for the Employees’
 # Provident fund (EPF) and Rs. 200 is deducted from the gross pay
 # as the professional tax.
+
+# B = basic salary 
+# DA = B * 0.97
+# HRA = B * 0.57
+# Medical allowance = 150
+# gross pay = B + DA + HRA + Medical allowance
+
+# EPF = B * 0.12
+# professional tax = 120 
+# The Net salary = gross pay - (EPF + professional tax)
+
+basic_salary = eval(input("Enter the basic salary is :"))
+DA = basic_salary * 0.97
+HRA = basic_salary * 0.57
+Medical_allowance= 150
+EPF= basic_salary * 0.12
+professional_tax = 200
+
+Gross_salary = basic_salary + DA + HRA + Medical_allowance
+The_net_salary = Gross_salary - (EPF + professional_tax)
+
+print("The Net salary is :",The_net_salary)
