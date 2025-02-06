d06d3da8444e54f5e77b5ee088469ffcfcc8a9e0
Ankita Das <adas22156@gmail.com>
2025-02-06 15:47:17 +0530

    hii

--- Exam/abbreviation.py
+++ Exam/abbreviation.py
@@ -1,3 +1,9 @@
 name = "ankita das roy".title().split()
 short_name = '.'.join([n[0].upper() for n in name[:-1]]) + '.' + name[-1]
 print(short_name)  # Output: A.D.Roy
+
+short_name = '.'.join([n[0].upper() for n in name[::]]) 
+print(short_name)  # Output: A.D.R
+
+short_name = ''.join([n[0].upper() for n in name[::]]) 
+print(short_name)  # Output: ADR
